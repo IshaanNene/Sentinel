9593430ed3d13e0355964b5b9a66f6b7ae262202
ISHAAN NENE <ishaannene@gmail.com>
2024-12-15 15:58:27 +0530

    Updates

--- internal/monitor/exporter.go
+++ internal/monitor/exporter.go
@@ -1,14 +1,37 @@
 package monitor
 
 import (
-	"pkg/exporters"
-	"/pkg/alerts"
+	"encoding/csv"
+	"encoding/json"
+	"os"
+	"fmt"
 )
 
 func ExportMetricsToCSV(metrics map[string]float64, filename string) error {
-	return exporters.ExportToCSV(metrics, filename)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for key, value := range metrics {
+		if err := writer.Write([]string{key, fmt.Sprintf("%f", value)}); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func ExportMetricsToJSON(metrics map[string]float64, filename string) error {
-	return exporters.ExportToJSON(metrics, filename)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(metrics)
 }
